10c9e982ef92791ec681610e4b109d7d2d2e031d
justYu2001 <jack4781012@gmail.com>
2021-08-18 18:09:44 +0800

    build: set public path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const resolve = (dir) => path.join(__dirname, dir);
 
 module.exports = {
+    publicPath:
+        process.env.NODE_ENV === 'production'
+            ? '/vue-mask-map/'
+            : '/',
     css: {
         loaderOptions: {
             scss: {
